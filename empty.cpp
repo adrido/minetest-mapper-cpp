ecc93602fc01b684e7edd09485340102d80116fb
Rogier <rogier777@gmail.com>
2016-06-25 09:10:13 +0200

    gcc6 fix: main must be a function now...
    The error was not in minetestmapper itself, but in the build infrastructure.

--- empty.cpp
+++ empty.cpp
@@ -1,6 +1,4 @@
-// File used by cmake to check for c++11 support
+// File used by cmake to check for c++ standard support
 // See: CmakeLists.txt
 
-// Just a symbol named 'main' is required for compilation to succeed.
-// It does not have to be a function :-)
-int main;
+int main(void) { return 0; }
