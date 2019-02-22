abd91ab507a8b40de3e01772660d29df7fbed5be
Unknown <robots_only_adrido@gmx.com>
2019-02-22 13:53:23 +0100

    Modernize Settings Class

--- Minetestmapper/Settings.cpp
+++ Minetestmapper/Settings.cpp
@@ -1,28 +1,23 @@
+#include "Settings.h"
 
 #include <iostream>
-#include "Settings.h"
 
 static std::string trim(const std::string &s)
 {
-    size_t start = s.find_first_not_of(" \t\r\n");
-    size_t end = s.find_last_not_of(" \t\r\n");
-    return s.substr(start, (end-start+1));
+	const size_t start = s.find_first_not_of(" \t\r\n");
+	const size_t end = s.find_last_not_of(" \t\r\n");
+	return s.substr(start, (end - start + 1));
 }
 
 Settings::Settings(const std::string &filename, const std::string &msgName)
-	:  m_filename(filename),
-	   m_messageName(msgName)
+	: m_filename(filename),
+	  m_messageName(msgName)
 {
-	if (m_messageName == "")
+	if (m_messageName.empty())
 		m_messageName = m_filename;
-	m_file.open(m_filename.c_str(), std::ifstream::in);
+	m_file.open(m_filename, std::ifstream::in);
 	if (!m_file.is_open())
 		throw std::runtime_error(std::string("Failed to open file '") + m_messageName + "'");
-}
-
-Settings::~Settings(void)
-{
-	m_file.close();
 }
 
 std::string Settings::get(const std::string &key)
@@ -50,9 +45,9 @@ bool Settings::getGeneric(const std::string &key, std::string *pvalue)
 	bool found = false;
 	m_file.clear();
 	m_file.seekg(0);
-	for (std::getline(m_file,line); m_file.good(); std::getline(m_file,line)) {
+	for (std::getline(m_file, line); m_file.good(); std::getline(m_file, line)) {
 		linenr++;
-		size_t keylen = line.find_first_of('=');
+		const size_t keylen = line.find_first_of('=');
 		if (keylen == std::string::npos) {
 			std::cerr << "Error parsing config line at " << m_filename << ":" << linenr << ": expected: <name> = <value> ('=' not found)";
 			continue;
@@ -65,4 +60,3 @@ bool Settings::getGeneric(const std::string &key, std::string *pvalue)
 	}
 	return found;
 }
-
--- Minetestmapper/Settings.h
+++ Minetestmapper/Settings.h
@@ -1,19 +1,19 @@
+#pragma once
 
-#ifndef _SETTINGS_H_
-#define _SETTINGS_H_
-
-#include <stdexcept>
-#include <sstream>
 #include <fstream>
+#include <sstream>
+#include <stdexcept>
 
-class Settings {
+class Settings
+{
 public:
 	Settings(const std::string &filename, const std::string &msgName = "");
-	~Settings(void);
+	~Settings() = default;
 	std::string get(const std::string &key);
 	std::string get(const std::string &key, const std::string &defaultValue);
 	bool check(const std::string &key) { return getGeneric(key); }
 	bool check(const std::string &key, std::string &value) { return getGeneric(key, &value); }
+
 private:
 	std::string m_filename;
 	std::string m_messageName;
@@ -21,5 +21,3 @@ private:
 
 	bool getGeneric(const std::string &key, std::string *pvalue = nullptr);
 };
-
-#endif // _SETTINGS_H_
